d50dc0b519600a0bbe985c41f9e070f958937faf
Juan Brand <juanbrand298155@correo.itm.edu.co>
2025-10-13 04:25:39 -0500

    Reintegrate the Frontend

---
+++ Employees/Employees.Frontend/Program.cs
@@ -0,0 +1,33 @@
+using Employees.Frontend.Components;
+using Employees.Frontend.Repositories;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7228") });
+builder.Services.AddScoped<IRepository, Repository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
