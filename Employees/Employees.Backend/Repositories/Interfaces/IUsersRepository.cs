0ba17f0315729af1606831dd78858b7f91d9cf9f
Juan Brand <juanbrand298155@correo.itm.edu.co>
2025-11-29 18:00:00 -0500

    Edit user added

--- Employees/Employees.Backend/Repositories/Interfaces/IUsersRepository.cs
+++ Employees/Employees.Backend/Repositories/Interfaces/IUsersRepository.cs
@@ -19,4 +19,10 @@ public interface IUsersRepository
     Task<SignInResult> LoginAsync(LoginDTO model);
 
     Task LogoutAsync();
+
+    Task<User> GetUserAsync(Guid userId);
+
+    Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+
+    Task<IdentityResult> UpdateUserAsync(User user);
 }
--- Employees/Employees.Backend/UnitsOfWork/Interfaces/IUsersUnitOfWork.cs
+++ Employees/Employees.Backend/UnitsOfWork/Interfaces/IUsersUnitOfWork.cs
@@ -19,4 +19,10 @@ public interface IUsersUnitOfWork
     Task<SignInResult> LoginAsync(LoginDTO model);
 
     Task LogoutAsync();
+
+    Task<User> GetUserAsync(Guid userId);
+
+    Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+
+    Task<IdentityResult> UpdateUserAsync(User user);
 }
