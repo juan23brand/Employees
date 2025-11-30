ab7a6e4f10abb2b2e89b8cb20a1c2a22eefb8ec5
Juan Brand <juanbrand298155@correo.itm.edu.co>
2025-11-29 19:09:58 -0500

    Added password change

--- Employees/Employees.Frontend/Components/Pages/Auth/ChangePassword.razor.cs
+++ Employees/Employees.Frontend/Components/Pages/Auth/ChangePassword.razor.cs
@@ -1,6 +1,41 @@
-namespace Employees.Frontend.Components.Pages.Auth
+using Employees.Frontend.Repositories;
+using Employees.Shared.DTOs;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Employees.Frontend.Components.Pages.Auth;
+
+public partial class ChangePassword
 {
-    public partial class ChangePassword
+    private ChangePasswordDTO changePasswordDTO = new();
+    private bool loading;
+
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private IDialogService DialogService { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [CascadingParameter] private IMudDialogInstance MudDialog { get; set; } = null!;
+
+    private async Task ChangePasswordAsync()
     {
+        loading = true;
+        var responseHttp = await Repository.PostAsync("/api/accounts/changePassword", changePasswordDTO);
+        loading = false;
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(message!, Severity.Error);
+            return;
+        }
+
+        MudDialog.Cancel();
+        NavigationManager.NavigateTo("/EditUser");
+        Snackbar.Add("Contraseña Modificada con éxito.", Severity.Success);
+    }
+
+    private void ReturnAction()
+    {
+        MudDialog.Cancel();
+        NavigationManager.NavigateTo("/EditUser");
     }
 }
